8578bd7f3fb065bbcdb4faecf520275ab1fa07c7
Harshit Kumar <128478587+HarshitK9@users.noreply.github.com>
2024-01-19 15:20:23 +0530

    Chnage

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 4000;
+const port = 8000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
